823076cd076c2bfd119ff34b3cd787d10b589cdf
Wilfried Musanzi <musanziw.m@gmail.com>
2026-09-15 10:43:12 +0200

    refactor(participations): remove unused participation query handler and query class

--- src/modules/participations/queries/impl/index.ts
+++ src/modules/participations/queries/impl/index.ts
@@ -1,4 +1,3 @@
 export * from './find-my-participations.query';
 export * from './find-participation-by-id.query';
-export * from './find-participation-by-participant-and-activity.query';
 export * from './find-participations.query';
